df2a16ee337a8913a9a0a6d142d19acbe810ae1c
nazmia-14 <nazmihibreash@gmail.com>
2021-03-18 15:15:42 +0200

    add js features

---
+++ app.js
@@ -0,0 +1,40 @@
+
+alert("welcome to van's gallery");
+
+//var age = 23;
+
+//console.log(age);//
+
+var username = prompt("what's your name?");
+
+console.log(username);
+
+document.write('<h3>' + "Hello" + username + '</h3>');
+
+var painting = prompt("what's your favourite van's painting? please choose between: Starry Night, Irises or Wheatfield ");
+
+if (painting == 'Starry Night') {
+    document.write('<style background-color: blue>');
+
+
+} else if (painting == 'Irises') {
+    document.write('<img src="4.jpg" >');
+} else if (painting == 'Wheatfield ') {
+    document.write('<img src="5.jpg" >');
+} else {
+    alert('choose betwen  Starry Night, Irises or Wheatfield ');
+}
+
+var backgroungcolor = prompt("what's your favourite color background ? please choose between: red, blue or pink ");
+
+if (painting == 'red') {
+    document.body.style.backgroundColor = "red";
+
+
+} else if (painting == 'blue') {
+    document.body.style.backgroundColor = "blue";
+} else if (painting == 'pink') {
+    document.body.style.backgroundColor = "pink";
+} else {
+    alert('sorry the back ground collor will not change ');
+}
